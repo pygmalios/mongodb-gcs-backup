82a151c3d5e7197c0c03ebe657936ef955b6ad5f
Ondrej Galbavy <ondrej.galbavy@gmail.com>
2025-03-16 10:47:04 +0100

    Use gcloud storage + shellcheck

--- backup.sh
+++ backup.sh
@@ -7,7 +7,6 @@ set -o nounset
 # set -o xtrace
 
 BACKUP_DIR=${BACKUP_DIR:-/tmp}
-BOTO_CONFIG_PATH=${BOTO_CONFIG_PATH:-/root/.boto}
 GCS_BUCKET=${GCS_BUCKET:-}
 GCS_KEY_FILE_PATH=${GCS_KEY_FILE_PATH:-}
 MONGODB_URI=${MONGODB_URI:-}
@@ -23,19 +22,21 @@ SLACK_CHANNEL=${SLACK_CHANNEL:-}
 SLACK_USERNAME=${SLACK_USERNAME:-}
 SLACK_ICON=${SLACK_ICON:-}
 
+export CLOUDSDK_AUTH_CREDENTIAL_FILE_OVERRIDE=$GCS_KEY_FILE_PATH
+
 backup() {
-  mkdir -p $BACKUP_DIR
+  mkdir -p "$BACKUP_DIR"
   date=$(date "+%Y-%m-%dT%H:%M:%SZ")
   archive_name="backup-$date.tar.gz"
 
   cmd_auth_part=""
-  if [[ ! -z $MONGODB_USER ]] && [[ ! -z $MONGODB_PASSWORD ]]
+  if [[ -n $MONGODB_USER ]] && [[ -n $MONGODB_PASSWORD ]]
   then
     cmd_auth_part="--username=\"$MONGODB_USER\" --password=\"$MONGODB_PASSWORD\""
   fi
 
   cmd_db_part=""
-  if [[ ! -z $MONGODB_DB ]]
+  if [[ -n $MONGODB_DB ]]
   then
     cmd_db_part="--db=\"$MONGODB_DB\""
   fi
@@ -46,7 +47,7 @@ backup() {
     cmd_oplog_part="--oplog"
   fi
 
-  if [ ! -z ${MONGODB_URI:-} ]; then
+  if [ -n "${MONGODB_URI:-}" ]; then
     cmd="mongodump --uri=\"$MONGODB_URI\" $cmd_db_part $cmd_oplog_part --gzip --archive=$BACKUP_DIR/$archive_name"
   else
     cmd="mongodump --host=\"$MONGODB_HOST\" --port=\"$MONGODB_PORT\" $cmd_auth_part $cmd_db_part $cmd_oplog_part --gzip --archive=$BACKUP_DIR/$archive_name"
@@ -57,22 +58,8 @@ backup() {
 }
 
 upload_to_gcs() {
-  if [[ $GCS_KEY_FILE_PATH != "" ]]
-  then
-cat <<EOF > $BOTO_CONFIG_PATH
-[Credentials]
-gs_service_key_file = $GCS_KEY_FILE_PATH
-[Boto]
-https_validate_certificates = True
-[GoogleCompute]
-[GSUtil]
-content_language = en
-default_api_version = 2
-[OAuth2]
-EOF
-  fi
   echo "uploading backup archive to GCS bucket=$GCS_BUCKET"
-  gsutil cp $BACKUP_DIR/$archive_name $GCS_BUCKET
+  gcloud storage cp "$BACKUP_DIR/$archive_name" "$GCS_BUCKET"
 }
 
 send_slack_message() {
@@ -80,7 +67,7 @@ send_slack_message() {
   local title=${2}
   local message=${3}
 
-  echo 'Sending to '${SLACK_CHANNEL}'...'
+  echo "Sending to '${SLACK_CHANNEL}'..."
   curl --silent --data-urlencode \
     "$(printf 'payload={"channel": "%s", "username": "%s", "link_names": "true", "icon_emoji": "%s", "attachments": [{"author_name": "mongodb-gcs-backup", "title": "%s", "text": "%s", "color": "%s"}]}' \
         "${SLACK_CHANNEL}" \
@@ -90,13 +77,13 @@ send_slack_message() {
         "${message}" \
         "${color}" \
     )" \
-    ${SLACK_WEBHOOK_URL} || true
+    "${SLACK_WEBHOOK_URL}" || true
   echo
 }
 
 err() {
   err_msg="Something went wrong on line $(caller)"
-  echo $err_msg >&2
+  echo "$err_msg" >&2
   if [[ $SLACK_ALERTS == "true" ]]
   then
     send_slack_message "danger" "Error while performing mongodb backup" "$err_msg"
@@ -104,7 +91,7 @@ err() {
 }
 
 cleanup() {
-  rm $BACKUP_DIR/$archive_name
+  rm "$BACKUP_DIR/$archive_name"
 }
 
 trap err ERR
